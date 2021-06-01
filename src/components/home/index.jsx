5cf873195e6eea8fdebda2a9ef027d75ad836dd2
Denílson Pereira <deene67@gmail.com>
2021-06-01 13:10:13 -0300

    aplicando make styles

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        backgroundColor: theme.palette.background.default
-    }
-}));
 
 const Home = () =>
 (
-    <Grid className={useStyles.root}>
-        <div>
-            Avaliação de Denílson
-            <Link to="/maxdoctor">
-                <button>
-                    Acessar MaxDoctor
+    <div>
+        Avaliação de Denílson
+        <Link to="/maxdoctor">
+            <button>
+                Acessar MaxDoctor
                 </button>
-            </Link>
-        </div>
-    </Grid>
+        </Link>
+    </div>
 );
 
 export default Home;
--- src/components/template/index.jsx
+++ src/components/template/index.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import Routes from '../../routes/index'
-import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         backgroundColor: theme.palette.background.default
+    },
+    main: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
     }
 }));
 
-const Base = () =>
-(
-    <Grid className={useStyles.root}>
-        <Routes />
-    </Grid>
-);
+const Base = props => {
 
-export default Base;
+    const classes = useStyles();
+
+    return (
+        <Container maxWidth="sm" className={classes.root}>
+            <main className={classes.main}>
+                <Routes />
+            </main>
+        </Container >
+    );
+}
+
+
+export default withStyles(useStyles)(Base);
