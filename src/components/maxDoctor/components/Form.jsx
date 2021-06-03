bdba8b640deed7ab1f145b78c382e5654df46e18
Denílson Pereira <deene67@gmail.com>
2021-06-02 23:02:26 -0300

    mensagem de sucesso

--- src/components/maxDoctor/components/Form.jsx
+++ src/components/maxDoctor/components/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { FormControl, Grid, TextField, Typography, InputAdornment, Button } from '@material-ui/core';
+import { FormControl, TextField, Typography, InputAdornment, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
     },
     typographySubtitle: {
         fontWeight: '549',
-        fontSize:'0.9rem',
+        fontSize: '0.9rem',
         textTransform: 'uppercase',
         color: theme.palette.primary.main
     },
@@ -19,25 +19,79 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '0px',
     },
     buttonSubmit: {
-        marginTop: '22px',
+        marginTop: '10px',
         marginBottom: '15px',
         minWidth: '100%'
+    },
+    select: {
+        paddingTop: '10px'
+    },
+    messsageSuccess: {
+        fontWeight: '549',
+        fontSize: '0.9rem',
+        textTransform: 'uppercase',
+        color: theme.palette.primary.main,
+        marginBottom: '0px',
+        paddingTop: '10px',
     }
 }));
 
 const Form = () => {
+
     const classes = useStyles();
     const [ufs, setUfs] = useState([]);
+
+    const [form, setForm] = useState(
+        {
+            crm: '',
+            uf: '0'
+        }
+    );
+
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const [formErrors, setFormErrors] = useState(false);
+
+    const handleChange = e => {
+        form[e.target.name] = e.target.value;
+        setForm(form);
+    }
+
+    const handleSubmit = () => {
+        if (validate())
+            setFormSubmitted(true)
+        else
+            setFormSubmitted(false)
+    }
+
+    const validate = () => {
+        let errors = {};
+
+        if (form.crm === '')
+            errors.crm = "Este campo é obrigatório";
+
+        if (form.crm !== '' && form.crm.length < 5)
+            errors.crm = "Necessário ter no mínimo 5 caracteres.";
+
+        if (form.uf === '0')
+            errors.uf = "Este campo é obrigatório";
+
+
+        setFormErrors(errors)
+
+        return Object.values(errors).every(x => x === "")
+    }
+
+    // Pegar Dados UF
+    useEffect(() => {
+        getUf();
+    }, []);
 
     const getUf = () => {
         fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
             .then(response => response.json())
             .then(ufsData => setUfs(ufsData));
     }
-
-    useEffect(() => {
-        getUf();
-    }, []);
 
     const adornment = text => (
         <InputAdornment position="start">
@@ -47,42 +101,58 @@ const Form = () => {
         </InputAdornment>
     )
 
+
     return (
         <FormControl fullWidth="true">
             <TextField
+
                 id="standard-start-adornment"
+                name="crm"
+                onChange={e => handleChange(e)}
+                defaultValue={form.crm}
                 placeholder="0000000"
                 InputProps={{
                     startAdornment: adornment("CRM")
                 }}
+                {...(formErrors.crm && { error: true, helperText: formErrors.crm })}
             />
 
             <TextField
+                name="uf"
                 className={classes.select}
-                id="standard-select-currency-native"
+                onChange={e => handleChange(e)}
                 select
-                // value={currency}
-                // onChange={handleChange}
+                id="standard-select-currency-native"
                 SelectProps={{
                     native: true,
                 }}
                 InputProps={{
                     startAdornment: adornment("UF")
                 }}
+                {...(formErrors.uf && { error: true, helperText: formErrors.uf })}
             >
-                <option value="0">
+                <option value="0" selected={form.uf === "option.sigla"}>
                     Selecionar
                 </option>
                 {ufs.sort((a, b) => a.sigla < b.sigla ? -1 : a.sigla > b.sigla ? 1 : 0).map((option) => (
-                    <option key={option.sigla} value={option.sigla}>
+                    <option selected={form.uf === option.sigla} key={option.sigla} value={option.sigla}>
                         {option.sigla}
                     </option>
                 ))}
             </TextField>
 
-            <Button variant="contained" color="secondary" className={classes.buttonSubmit}>
+            {
+                formSubmitted &&
+                <Typography className={classes.messsageSuccess} variant="subtitle1" gutterBottom>
+                    <p className={classes.title}>sucesso</p>
+                </Typography>
+            }
+
+            <Button onClick={() => handleSubmit()} variant="contained" color="secondary" className={classes.buttonSubmit}>
                 Continue
             </Button>
+
+
 
             <Typography className={classes.typographySubtitle} variant="subtitle1" gutterBottom>
                 <p className={classes.title}>Canal de compra exclusico </p>
